1d5cfb95b99686d6430108b129fcb1d7b6e55e8d
Caleb Brown <dizziboi07@gmail.com>
2024-08-26 08:50:28 -0400

    added coding practice

---
+++ fav/favcolor.java
@@ -0,0 +1,11 @@
+package fav;
+
+public class favcolor 
+{
+
+ public static void main(String[] args) {
+    String name = "Mary";
+        String color = "blue";
+        System.out.println(name + "'s favorite color is " + color);
+ }   
+}
---
+++ fav/favsport.java
@@ -0,0 +1,10 @@
+package fav;
+
+public class favsport {
+    public static void main(String[] args) {
+        String name = "Gabby";
+        String sport = "soccer";
+        System.out.println(name + "'s favorite sport is " + sport);
+    }
+    
+}

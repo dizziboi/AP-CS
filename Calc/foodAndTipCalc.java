16dcf19312f4efda2262847d4a16c646bc93967e
caleb <dizziboi07@gmail.com>
2024-08-28 10:13:11 -0400

    another calc and problem

--- Calc/foodAndTipCalc.java
+++ Calc/foodAndTipCalc.java
@@ -3,7 +3,6 @@ package Calc;
 public class foodAndTipCalc {
     public static void main(String[] args) {
         double bill = 89.23;
-        
         double tip = bill * 0.20;
         double total = bill + tip;
         int numPeople = 3;

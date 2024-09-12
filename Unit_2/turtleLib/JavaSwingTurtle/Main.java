f5fe5a170cefc204910c98080732e812b9942e97
Caleb Brown <dizziboi07@gmail.com>
2024-09-12 09:13:30 -0400

    tried to make a triangle

--- Unit_2/turtleLib/JavaSwingTurtle/Main.java
+++ Unit_2/turtleLib/JavaSwingTurtle/Main.java
@@ -4,14 +4,15 @@ import java.awt.*;
 
 public class Main {
   public static void main(String[] args) {  
-    World world = new World(400,400);
+    World world = new World(800,600);
     Turtle yertle = new Turtle(world);
     // Add your own code in here 
-    yertle.setColor(Color.blue);
-    yertle.forward();
-    yertle.turnRight();
-    yertle.forward();
-    
+    yertle.setColor(Color.red);
+    //yertle.turnRight();
+    yertle.turn(90);
+    yertle.turn(120);
+    //yertle.turn(90);
+ 
     world.setVisible(true);
   }
 }

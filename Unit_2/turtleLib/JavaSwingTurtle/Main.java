93472bfbf01b2f199e740e9cf1c194d88df32ab9
Caleb Brown <dizziboi07@gmail.com>
2024-09-16 13:29:17 -0400

    cleared main and new turtle project

--- Unit_2/turtleLib/JavaSwingTurtle/Main.java
+++ Unit_2/turtleLib/JavaSwingTurtle/Main.java
@@ -7,12 +7,5 @@ public class Main {
     World world = new World(800,600);
     Turtle yertle = new Turtle(world);
     // Add your own code in here 
-    yertle.setColor(Color.red);
-    //yertle.turnRight();
-    yertle.turn(90);
-    yertle.turn(120);
-    //yertle.turn(90);
- 
-    world.setVisible(true);
   }
 }

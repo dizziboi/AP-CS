26e7b147a192649c271864c5033ff48e603050d6
Caleb Brown <dizziboi07@gmail.com>
2024-09-17 01:11:48 -0400

    finished remainder code

--- Unit_1/cashRegister/remainder.java
+++ Unit_1/cashRegister/remainder.java
@@ -7,19 +7,30 @@ import java.math.*;
 public class remainder {
     public static void main(String[] args) {
 
-        Scanner priceInput = new Scanner(System.in); //makes the scanner object for the price input
-        System.out.println("Enter the price");
-        Double price = priceInput.nextDouble(); //reads the scanner 
+        Scanner priceInput1 = new Scanner(System.in); //makes the scanner object for the price input
+        System.out.println("Item 1 Price");
+        Double price1 = priceInput1.nextDouble(); //reads the scanner 
+
+        Scanner priceInput2 = new Scanner(System.in); //makes the scanner object for the price input
+        System.out.println("Item 2 Price");
+        Double price2 = priceInput2.nextDouble(); //reads the scanner 
+
+        Scanner priceInput3 = new Scanner(System.in); //makes the scanner object for the price input
+        System.out.println("Item 3 Price");
+        Double price3 = priceInput3.nextDouble(); //reads the scanner 
 
         Scanner paidInput = new Scanner(System.in); //makes the scanner object for the paid input
         System.out.println("Enter the amount paid");
         Double amount = paidInput.nextDouble(); //reads the scanner 
+
+        double priceTotal;
+         priceTotal = price1 + price2 + price3; 
         
         double total;
-         total = amount - price; //subtracts the amount from the price
+         total = amount - priceTotal; //subtracts the amount from the price
 
          double roundOff = Math.round(total * 100.0) / 100.0; //rounds up the total 
 
-        System.out.println("Change owed $" + roundOff); // output my response 
+        System.out.println("Change owed:$" + roundOff); // output my response 
     }
 }

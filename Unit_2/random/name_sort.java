1570bdc9733f7d672ef416b08e0a8dfa1af5a1a8
caleb <dizziboi07@gmail.com>
2024-09-30 13:28:05 -0400

    began work on name program

---
+++ Unit_2/random/name_sort.java
@@ -0,0 +1,18 @@
+//caleb brown
+//due 10/3/24
+//day submitted 
+//pig Latin-Name Lab 
+
+package Unit_2.random;
+import java.util.Scanner;
+import java.math.*;
+
+public class name_sort {
+    public static void main(String[] args) {
+        Scanner nameInput = new Scanner(System.in); //makes the scanner object for the price input
+        System.out.println("What is your name ");
+        Double name = nameInput.nextDouble(); //reads the scanner 
+        System.out.println(name);  
+    }
+    
+}

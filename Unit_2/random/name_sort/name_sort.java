03929a4f10f3f9631496dddd42c18f3ac7616fdb
Caleb Brown <dizziboi07@gmail.com>
2024-10-02 10:48:22 -0400

    finished name sorting code

--- Unit_2/random/name_sort/name_sort.java
+++ Unit_2/random/name_sort/name_sort.java
@@ -16,22 +16,23 @@ public class name_sort {
         System.out.println("what is your name"); //prints out the question
         String fullName = myName.nextLine(); //reads the scanner 
         int spaceFinder = fullName.indexOf(" ");// detects the first space in the name
-        String firstName = fullName.substring(0,spaceFinder);
+        String firstName = fullName.substring(0,spaceFinder); // finds the first name
         System.out.println("First Name: " + firstName); //prints out the first name
+      
         fullName = fullName.replaceFirst(firstName, ""); //removes the first name from the full name String 
-        String fullNameTrimed = fullName.replaceAll(" ", "()"); //resmoves the spaces
-        int spaceFinder2 = fullNameTrimed.indexOf("()");// detects the second space in the name
+        int middleLastLength = fullName.length(); //finds the length of the name 
+        String fullNameTrimed = fullName.substring(1, middleLastLength); //resmoves front the space
+        int spaceFinder2 = fullNameTrimed.indexOf(" ");// detects the second space in the name 
+        String middleName = fullNameTrimed.substring(0,spaceFinder2); //finds the middle name
+        System.out.println("Middle name: "+ middleName); //prints the middle name
+        fullNameTrimed = fullNameTrimed.replaceFirst(middleName, ""); //removes the middle name
         
-        String middleName = fullName.substring(0,spaceFinder2);
-        System.out.println("middle name:"+ middleName);
-
-        System.out.println(fullNameTrimed);
-        System.out.println(spaceFinder2);
-
-
+        int lastLength = fullNameTrimed.length(); //finds the length of the name 
+        fullNameTrimed = fullNameTrimed.substring(1, lastLength); //resmoves front the space
+        String lastName = fullNameTrimed; 
+        System.out.println("Last name: " + lastName); //prints out the last name
         return firstName;
-        
-        
+
     }
      public static String pigLatin(){
         return null;

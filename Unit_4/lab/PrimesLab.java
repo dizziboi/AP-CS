ed9f745b3c91f713addf0b8ce31cfa1724dbb806
Caleb Brown <dizziboi07@gmail.com>
2024-11-14 20:51:01 -0500

    more code on the prime lab

--- Unit_4/lab/PrimesLab.java
+++ Unit_4/lab/PrimesLab.java
@@ -1,10 +1,23 @@
 package Unit_4.lab;
+import java.util.Scanner;
 
 public class PrimesLab {
  
     public static int countFactors(){
+        Scanner primeInput = new Scanner(System.in); //makes the scanner object for the price input
+        System.out.println("Type in a number ");
+        int number = primeInput.nextInt(); //reads the scanner 
+        int count = 0;
+        System.out.println(number);
+        while ((number % 2 >= count)) {
+            count++;
             
-        
+
+        }
+        System.out.print(count);
         return 0;
+    }
+    public static void main(String[] args) {
+        countFactors();
     }
 }

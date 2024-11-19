5291be9d46e50c77a2e7a5a386ee154f5bbdfff6
Caleb Brown <dizziboi07@gmail.com>
2024-11-19 13:14:49 -0500

    added mutiple incomplete methods to PrimesLab

--- Unit_4/lab/PrimesLab.java
+++ Unit_4/lab/PrimesLab.java
@@ -16,10 +16,51 @@ public class PrimesLab {
             factor++;
             
         }
-        System.out.print(counter);
-        return factor;
+        System.out.println(counter);
+        return counter;
+  
     }
+    public static boolean isPrime() {
+       int factor = countFactors();    
+       if (factor <= 2){
+        System.out.println("true");
+       } else{
+        System.out.println("false");
+       }
+        return false;
+    }
+
+    public static int countPrimes(){
+        
+       if(isPrime()){
+
+        }
+        return 0;
+        
+    }
+
+    public static int fourHeads(){
+    boolean t = true;
+    int hCount = 0;
+    int tCount = 0;
+    String line = "";
+    boolean fourHeads = false;
+        while (hCount <= 3){
+        int coinFlip =  (int) (Math.random() +.5 );
+        if (coinFlip == 0){
+            line= line + "H";
+            hCount++;
+        } else {
+            line= line + "T";
+        }
+        System.out.print(line);
+    }
+            return 0;
+    }
+
+    
     public static void main(String[] args) {
-        countFactors();
+        //isPrime();
+        fourHeads();
     }
 }

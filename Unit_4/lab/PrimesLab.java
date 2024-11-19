501d64e1a6161a31b6e14e61b9ed59cedb2701d4
Caleb Brown <dizziboi07@gmail.com>
2024-11-18 22:28:15 -0500

    faild at making lab work

--- Unit_4/lab/PrimesLab.java
+++ Unit_4/lab/PrimesLab.java
@@ -6,16 +6,19 @@ public class PrimesLab {
     public static int countFactors(){
         Scanner primeInput = new Scanner(System.in); //makes the scanner object for the price input
         System.out.println("Type in a number ");
-        int number = primeInput.nextInt(); //reads the scanner 
-        int count = 0;
-        System.out.println(number);
-        while (number % 2 < 2 &&  count > number) {
-            count++;
-            
+        int numInput = primeInput.nextInt(); //reads the scanner 
+        int factor = 1;
+        int factorCount = 1;
+        while (factor<=numInput) {
+            if(numInput%factor==0){
+                System.out.println("Found factor: "+factor);
+            }
+            factor++;
+            factorCount++;
 
         }
-        System.out.print(count);
-        return 0;
+        System.out.print(factorCount);
+        return factor;
     }
     public static void main(String[] args) {
         countFactors();

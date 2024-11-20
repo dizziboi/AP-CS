7aff25536174dab08a26d2b4d121053f03781076
Caleb Brown <dizziboi07@gmail.com>
2024-11-19 20:16:56 -0500

    added scanner template for future use

---
+++ random/Scanner_Template.java
@@ -0,0 +1,12 @@
+package random;
+
+import java.util.Scanner;
+
+public class Scanner_Template {
+    public static void main(String[] args) {
+        Scanner input_placeholder = new Scanner(System.in); //makes the scanner object for the *scanner* input
+        System.out.println("Type in a number ");
+        int input_placeholder2 = input_placeholder.nextInt(); //reads the scanner 
+    }
+            
+}

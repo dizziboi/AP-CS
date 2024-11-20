3a527789cb68a3b9894ea3fa90c8e41051ccfe0d
Caleb Brown <dizziboi07@gmail.com>
2024-11-19 22:39:33 -0500

    game where you need to choose a random number and a program that counts 1 though

---
+++ Unit_4/While/GuessingGame.java
@@ -0,0 +1,31 @@
+package Unit_4.While;
+
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+    // Choose a random number from 0-100
+        Scanner scan = new Scanner(System.in);
+        int numGuess = 0;
+        while (numGuess < 7) {
+        int randNumb =(int) (100*(Math.random()));
+        //System.out.println(randNumb); // prints answer
+            System.out.println("Type in a number ");
+            int guess = scan.nextInt(); //reads the scanner 
+            if (guess > randNumb){
+                System.out.println("Too high!");
+                numGuess++;
+                System.out.println(7 - numGuess + " Guesses left");
+            } else if (guess < randNumb){
+                System.out.println("Too Loo!");
+                numGuess++;
+                System.out.println(7 - numGuess + " Guesses left");
+            } else if (guess == randNumb){
+                System.out.println("You got it");
+            } else if (numGuess < 8){
+                System.out.println("");
+                numGuess = 0;
+            }
+        }
+    }
+}
---
+++ Unit_4/While/Looptest2.java
@@ -0,0 +1,13 @@
+package Unit_4.While;
+
+public class Looptest2 {
+    public static void main(String[] args)
+    {
+        int count = 0;
+        while (count < 8)
+        {
+            count++;
+            System.out.println(count);
+        }
+    }
+}

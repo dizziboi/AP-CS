e540be7a956a0982c350c3287b01d285f6c0ef98
Caleb Brown <dizziboi07@gmail.com>
2024-12-01 20:46:37 -0500

    program that replaces the word cat with dog

--- Unit_4/For/ihatecats.java
+++ Unit_4/For/ihatecats.java
@@ -1,15 +1,25 @@
 package Unit_4.For;
 
 public class ihatecats {
-    public static void main(String[] args) {
-        String message =
-                "I love cats! I have a cat named Coco. My cat's very smart!";
+    public static void main(String[] args)
+    {
+        String message = "I love cats! I have a cat named Coco. My cat's very smart!";
+        int index = 0;
 
-        while (message.indexOf("cats") <= 0) //sees if the word cat is in the sentence  
+
+
+        // while more 1's in the message
+        while (message.indexOf("cat") >= 0)
         {
-            message = message.replaceAll("1","dog");
-            
+            // Find the next index for 1
+            index = message.indexOf("cat");
+            // Replace the 1 with a l at index by concatenating substring up to
+            // index and then the rest of the string.
+            String firstpart = message.substring(0, index);
+            String lastpart = message.substring(index + 3);
+            message = firstpart + "dog" + lastpart;
+
         }
-        
+        System.out.println("Cleaned text: " + message);
     }
 }

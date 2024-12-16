5a01ae87b314f9f78714ad795acd780f79320db7
Caleb Brown <dizziboi07@gmail.com>
2024-12-16 07:12:48 -0500

    incomplete rpg character

--- Unit_4/RpgGame.java
+++ Unit_4/RpgGame.java
@@ -3,44 +3,48 @@ package Unit_4;
 import java.util.Scanner;
 
 public class RpgGame {
-    public static double rpgGame(){
-        Scanner character_Name = new Scanner(System.in); //makes the scanner object for the *scanner* input
-        System.out.println("What is your name ");
-        String name = character_Name.nextLine(); //reads the scanner 
-        
-        Scanner character_Race = new Scanner(System.in); //makes the scanner object for the *scanner* input
-        System.out.println("What is your race ");
-        String race = character_Race.nextLine(); //reads the scanner
-        
-        Scanner character_Job = new Scanner(System.in); //makes the scanner object for the *scanner* input
-        System.out.println("What is your job/class ");
-        String job = character_Job.nextLine(); //reads the scanner
-        
-        System.out.println("Name: " + name + "\n" + "Race: " + race + "\n" + "Job/class: " + job);
-  
-        return 0;
-        
-    }
-    public static int roll(int dieSides){ // rolls a dice with the number of sides determined by dieSides
-        int rollnum = (int) (dieSides * Math.random()) + 1;
-        //System.out.println(rollnum);
-        return rollnum;
-    }
-    public static int dieRollsStats(){
-        int roll1 = roll(6);
-        int roll2 = roll(6);
-        int roll = roll1 + roll2 + 6;
-        //System.out.println(roll);
-        return roll;
-    }
-    public static int setStats(){ //this will sets the stats for your character
-        int level = 1;
-        int strenght = 1;
-        int agility = 1;
-        int intelligence = 1;
-        int hit_points = 1;
-        int armor_class = 1;
-        int mana_power = 1;
+   public String name;
+    static String race;  
+    static String job ;
+    static int level = 1;
+    static int strenght = 1;
+    static int agility = 1;
+    static int intelligence = 1;
+    static int hit_points = 1;
+    static int armor_class = 1;
+    static int mana_power = 1;
+    
+        public static double rpgGame(){
+            Scanner character_Name = new Scanner(System.in); //makes the scanner object for the *scanner* input
+            System.out.println("What is your name ");
+            String name = character_Name.nextLine(); //reads the scanner 
+            
+            Scanner character_Race = new Scanner(System.in); //makes the scanner object for the *scanner* input
+            System.out.println("What is your race ");
+            String race = character_Race.nextLine(); //reads the scanner
+            
+            Scanner character_Job = new Scanner(System.in); //makes the scanner object for the *scanner* input
+            System.out.println("What is your job/class ");
+            String job = character_Job.nextLine(); //reads the scanner
+            
+            
+      
+            return 0;
+            
+        }
+        public static int roll(int dieSides){ // rolls a dice with the number of sides determined by dieSides
+            int rollnum = (int) (dieSides * Math.random()) + 1;
+            //System.out.println(rollnum);
+            return rollnum;
+        }
+        public static int dieRollsStats(){
+            int roll1 = roll(6);
+            int roll2 = roll(6);
+            int roll = roll1 + roll2 + 6;
+            //System.out.println(roll);
+            return roll;
+        }
+        public static int setStats(){ //this will sets the stats for your character
         strenght = level + dieRollsStats();
         agility = level + dieRollsStats();
         intelligence = level + dieRollsStats();
@@ -50,11 +54,16 @@ public class RpgGame {
         
         return 0;
     }
+    public static String printstatus(){
+        System.out.println("Name: " + name + "\r\n" + "Race: " + race + "\r\n" + "Job/Class: " + job);
+        return null;
+    }
 
     public static void main(String[] args) {
     //roll(10);
-    dieRollsStats();
-    //pgGame();
+    //dieRollsStats();
+    rpgGame();
+    printstatus();
 }
     
 }

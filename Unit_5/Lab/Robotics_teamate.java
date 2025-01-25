cbf1bd406255aa422630c9c2266b55022c2b37db
Caleb Brown <dizziboi07@gmail.com>
2025-01-24 19:22:08 -0800

    finished student and added package to rob teammatw

--- Unit_5/Lab/Robotics_teamate.java
+++ Unit_5/Lab/Robotics_teamate.java
@@ -1,4 +1,4 @@
-
+package Lab;
 
 public class Robotics_teamate {
     private static String name;
--- Unit_5/constructor/Student.java
+++ Unit_5/constructor/Student.java
@@ -6,22 +6,34 @@ package constructor;
 public class Student
 {
     // Write 4 instance variables
-    private String name;
     private int age;
     private int grade;
     private double gpa;
+    private String club;
     // Write a constructor with 4 parameters to initialize all of the instance
     // variables
-
+    public Student(int studentAge, int studentGrade, double studnetGpa, String studentClub){
+        age = studentAge;
+        grade = studentGrade;
+        gpa = studnetGpa;
+        club = studentClub;
+    }
     // Write a print method that prints all the instance variables
-    public void print() {}
+    public void print() {
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("Gpa: " + gpa);
+        System.out.println("Clubs: " + club);
+    }
 
     // main method
     public static void main(String[] args)
     {
         // Construct 2 Student objects using the constructor with different values
-
+        Student caleb = new Student(17,88,3.5,"tsa and frc");
+        Student jude = new Student(18,95,4.0,"bsu and tsa");
         // call their print() methods
-
+        caleb.print();
+        jude.print();
     }
 }

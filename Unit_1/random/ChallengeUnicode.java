8a8e2b5b7a06f2c1dbe1e26d7dda6173a21e60c0
Caleb Brown <dizziboi07@gmail.com>
2025-01-28 09:20:39 -0800

    changed old unicode code

--- Unit_1/random/ChallengeUnicode.java
+++ Unit_1/random/ChallengeUnicode.java
@@ -4,9 +4,9 @@ public class ChallengeUnicode
     public static void main(String[] args)
     {
         System.out.println(
-                "'A' in ASCII and Unicode: " + Character.toString(65));
-        System.out.println("Chinese for 'sun': " + Character.toString(11932));
-        System.out.println("A smiley emoji: " + Character.toString(128512));
+                "'A' in ASCII and Unicode: " + Character.toString((char) 65));
+        System.out.println("Chinese for 'sun': " + Character.toString((char) 11932));
+        System.out.println("A smiley emoji: " + Character.toString((char) 128512));
 
         // Old style. Doesn't work for all codepoints.
         System.out.println("This also works: " + (char) 65);

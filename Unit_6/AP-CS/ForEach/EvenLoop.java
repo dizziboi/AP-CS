0201495ff9eb97f85133ad1c78ae106d8151f5e7
Caleb Brown <dizziboi07@gmail.com>
2025-03-01 03:07:56 -0500

    more work that's not finished

---
+++ Unit_6/AP-CS/ForEach/EvenLoop.java
@@ -0,0 +1,17 @@
+package ForEach;
+
+public class EvenLoop
+{
+    public static void main(String[] args)
+    {
+        int[] values = {6, 2, 1, 7, 12, 5};
+        // Rewrite this loop as a for each loop and run
+        for (int i : values)
+        {
+            if (values[i] % 2 == 0)
+            {
+                System.out.println(values[i] + " is even!");
+            }
+        }
+    }
+}

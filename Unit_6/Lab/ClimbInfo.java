22c7ed79dc6a41ae94a636329fbf2597e52fb540
Caleb Brown <dizziboi07@gmail.com>
2025-03-09 22:37:01 -0400

    lab stuff

---
+++ Unit_6/Lab/ClimbInfo.java
@@ -0,0 +1,22 @@
+package Unit_6.Lab;
+
+public class ClimbInfo
+{
+/** Creates a ClimbInfo object with name peakName and time climbTime.
+* @param peakName the name of the mountain peak
+* @param climbTime the number of minutes taken to complete the climb
+*/
+public ClimbInfo(String peakName, int climbTime)
+{ /* implementation not shown */ }
+/** @return the name of the mountain peak
+*/
+public String getName()
+{
+    return null; /* implementation not shown */ }
+/** @return the number of minutes taken to complete the climb
+*/
+public int getTime()
+{
+    return 0; /* implementation not shown */ }
+// There may be instance variables, constructors, and methods that are not shown.
+}

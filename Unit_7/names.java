101cf48839a912e84c66f145f9e77bc6b287a994
Caleb Brown <dizziboi07@gmail.com>
2025-03-11 13:27:48 -0400

    new unit and stuff

---
+++ Unit_7/names.java
@@ -0,0 +1,19 @@
+package Unit_7;
+
+import java.util.ArrayList;
+
+public class names {
+    public static void main(String[] args)
+    {
+        ArrayList<String> nameList = new ArrayList<String>();
+        nameList.add("Diego");
+        nameList.add("Grace");
+        nameList.add("Deja");
+        System.out.println(nameList);
+        nameList.set(0, "Caleb");
+        System.out.println(nameList.get(0));
+        System.out.println(nameList.get(1));
+        System.out.println(nameList.get(2));
+        System.out.println(nameList);
+    }
+}

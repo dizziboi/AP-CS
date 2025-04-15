1699db4c4f1f3a3ff27517b496bc6034ce25988e
Caleb Brown <dizziboi07@gmail.com>
2025-04-14 21:58:47 -0400

    late unit 8 code

--- Unit_8/Lab/U8L1.java
+++ Unit_8/Lab/U8L1.java
@@ -1,18 +1,27 @@
 package Unit_8.Lab;
 
-public class Test {
+public class U8L1 {
         public static int sum(int[][] a){
             int sum = 0;
             for(int[] row: a){
                 for(int value: row)
                     sum += value;
             }
+            System.out.println(sum);
             return sum;
         }        
+
+        public static int rowSum(int[][] a, int b){
+            int sum = 0;
+            for(int[] row: a){
+                for(int value: row)
+                    sum += value; 
+                }
+            return sum;
+        }
     
     public static void main(String[] args) {
         int [][] matrix = {{1,2,3},{4,5,6}};
         sum(matrix);
-        System.out.print(matrix);
     }
 }
